c5ce9bbd6c700fa6ad48968f1aaff801cdc40464
Matthew <Bean6754@users.noreply.github.com>
2018-07-15 23:21:22 +0100

    Create complete_debloat.ps1

---
+++ complete_debloat.ps1
@@ -0,0 +1,24 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) 
+    {
+        # tried to elevate, did not work, aborting
+    } 
+    else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+
+exit
+}
+
+'running with full privileges'
+
+# Warning! This script will remove all Appx-Packages (UWP) from your system.
+
+Get-AppxPackage | Remove-AppxPackage
