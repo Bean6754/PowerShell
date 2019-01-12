80d409c8fb498e5572e0e97c09bdddabb7831160
Matthew <Bean6754@users.noreply.github.com>
2019-01-12 18:34:12 +0000

    Delete InstallFeature.ps1

--- Server/InstallFeature.ps1
+++
@@ -1,3 +0,0 @@
-Install-WindowsFeature -Name BitLocker
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
-# Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
