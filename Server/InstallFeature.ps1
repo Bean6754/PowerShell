1f09e1c5c1e86885d1353f3b3165218cf7ca8ec9
Matthew <Bean6754@users.noreply.github.com>
2019-01-12 15:12:02 +0000

    Update InstallFeature.ps1

--- Server/InstallFeature.ps1
+++ Server/InstallFeature.ps1
@@ -1,2 +1,3 @@
 Install-WindowsFeature -Name BitLocker
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
+# Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
