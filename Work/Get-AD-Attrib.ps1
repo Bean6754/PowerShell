a92460bc2edb907a93e79c8d2d00d53bb7c12d1b
Isabella <Bean6754@users.noreply.github.com>
2023-02-02 10:15:12 +0000

    Update Get-AD-Attrib.ps1

--- Work/Get-AD-Attrib.ps1
+++ Work/Get-AD-Attrib.ps1
@@ -1,3 +1,4 @@
+$staticAttributes = @("AccountExpirationDate","extensionAttribute13","extensionAttribute14","manager","enabled","mail","mailnickname","proxyaddresses","whencreated","whenChanged","passwordNeverExpires","passwordLastSet","employeeNumber","targetaddress","info","company","accountexpires","LastLogon","description","lastlogondate","office","physicalDeliveryOfficeName","extensionAttribute5","extensionAttribute13","memberof","title","legacyExchangeDN","AccountLockoutTime","LockedOut","LastBadPasswordAttempt","department","division","msExchRemoteRecipientType","office","msExchArchiveGUID","HomeDirectory","DisplayName","employeeid")
 $inputDomain = Read-Host -Prompt "Enter full-domain: (E.G: domain.name) "
 
 if (($inputDomain -eq "") -Or (!$inputDomain))
@@ -7,13 +8,17 @@ if (($inputDomain -eq "") -Or (!$inputDomain))
     try
     {
         $inputUsername = Read-Host -Prompt "Enter username: "
-        Get-ADUser $inputUsername -Server $inputDomain -Properties EmployeeID,EmployeeNumber
+        Get-ADUser $inputUsername -Server $inputDomain -Properties $staticAttributes
+        Write-Host "`n`n======================================`nExpanded AD attribute 'proxyAddresses' `n======================================`n"
+        Get-ADUser $inputUsername -Server $inputDomain -Properties proxyaddresses | Select-Object -ExpandProperty proxyaddresses | Out-Host
         Remove-Variable inputUsername
     }
     catch
     {
         $inputFullname = Read-Host -Prompt "Enter full-name: "
-        Get-ADUser -Server $inputDomain -Filter {Name -eq $inputFullname} -Properties EmployeeID,EmployeeNumber
+        Get-ADUser -Server $inputDomain -Filter {Name -eq $inputFullname} -Properties $staticAttributes
+        Write-Host "`n`n======================================`nExpanded AD attribute 'proxyAddresses' `n======================================`n"
+        Get-ADUser $inputUsername -Server $inputDomain -Properties proxyaddresses | Select-Object -ExpandProperty proxyaddresses | Out-Host
         Remove-Variable inputFullname
     }
 }
@@ -22,13 +27,17 @@ else
     try
     {
         $inputUsername = Read-Host -Prompt "Enter username: "
-        Get-ADUser $inputUsername -Server $inputDomain -Properties EmployeeID,EmployeeNumber
+        Get-ADUser $inputUsername -Server $inputDomain -Properties $staticAttributes
+        Write-Host "`n`n======================================`nExpanded AD attribute 'proxyAddresses' `n======================================`n"
+        Get-ADUser $inputUsername -Server $inputDomain -Properties proxyaddresses | Select-Object -ExpandProperty proxyaddresses | Out-Host
         Remove-Variable inputUsername
     }
     catch
     {
         $inputFullname = Read-Host -Prompt "Enter full-name: "
-        Get-ADUser -Server $inputDomain -Filter {Name -eq $inputFullname} -Properties EmployeeID,EmployeeNumber
+        Get-ADUser -Server $inputDomain -Filter {Name -eq $inputFullname} -Properties $staticAttributes
+        Write-Host "`n`n======================================`nExpanded AD attribute 'proxyAddresses' `n======================================`n"
+        Get-ADUser $inputUsername -Server $inputDomain -Properties proxyaddresses | Select-Object -ExpandProperty proxyaddresses | Out-Host
         Remove-Variable inputFullname
     }
 }
